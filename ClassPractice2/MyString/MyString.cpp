415002ce4054c102fd5fe77b082920a1a9476404
JuwonCha <juwon1512@gmail.com>
2022-04-01 18:15:55 +0900

    Edit MyString header, sourcefile

--- ClassPractice2/MyString/MyString.cpp
+++ ClassPractice2/MyString/MyString.cpp
@@ -1,5 +1,21 @@
 #include "MyString.h"
 
+MyString::MyString(const char* string) : mString{ string }
+{
+	// string 문자열의 길이 만큼 동적할당
+	mString = new const char[strlen(string) + 1]{ };
+	// 모든 문자열의 가장 뒤에는 \0
+
+	// string에서 멤버로 복사
+	strcpy_s((char*)mString, strlen(string) + 1, string);
+}
+
+MyString::~MyString()
+{
+	delete[] mString;
+	mString = nullptr;
+}
+
 void MyString::Print()
 {
 	std::cout << mString << std::endl;
--- ClassPractice2/MyString/MyString.h
+++ ClassPractice2/MyString/MyString.h
@@ -6,24 +6,10 @@ class MyString
 {
 private:
 	 const char* mString;
-	// 문자열 복사
 
 public:
-	MyString(const char* string) : mString{ string }
-	{
-		// string 문자열의 길이 만큼 동적할당
-		mString = new const char[strlen(string)+1]{ };
-		// 모든 문자열의 가장 뒤에는 \0
-
-		// string에서 멤버로 복사
-		strcpy_s((char*)mString, strlen(string)+1, string);
-	}
-
-	~MyString()
-	{
-		delete[] mString;
-		mString = nullptr;
-	}
+	MyString(const char* string);
+	~MyString();
 
 public:
 	void Print();
