566ff72197207ae8552b4deeb876d78bc62780eb
JuwonCha <juwon1512@gmail.com>
2022-04-01 17:52:48 +0900

    Add ClassPractice2

---
+++ ClassPractice2/MyString/MyString.cpp
@@ -0,0 +1,6 @@
+#include "MyString.h"
+
+void MyString::Print()
+{
+	std::cout << mString << std::endl;
+}
---
+++ ClassPractice2/MyString/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "MyString.h"
+
+int main()
+{
+	MyString s1{"HelloWorld"};
+
+	s1.Print();
+
+}
