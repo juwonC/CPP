d5ea31befdb794895cd066871092ab483585212e
JuwonCha <juwon1512@gmail.com>
2022-04-17 22:38:56 +0900

    Add STL Containers Array, Foward_list, List

--- STL_Practice2/STL_Practice2.cpp
+++ STL_Practice2/STL_Practice2.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <array>
+#include <forward_list>
+#include <list>
 
 int IntCompare(const void* left, const void* right)
 {
@@ -21,31 +24,148 @@ int IntCompare(const void* left, const void* right)
 
 int main()
 {
-    std::vector<int> v{ 4, 2, 3, 1, 5, 7, 6 };
+    //std::vector<int> v{ 4, 2, 3, 1, 5, 7, 6 };
 
-    qsort(v.data(), v.size(), sizeof(v[0]), IntCompare);
+    //qsort(v.data(), v.size(), sizeof(v[0]), IntCompare);
 
-    for (int value : v)
+    //for (int value : v)
+    //{
+    //    std::cout << value << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //std::vector<int> v1{ 1, 3, 2, 5, 4 };
+
+    //qsort(v1.data(), v1.size(), sizeof(v1[0]), [](const void* a, const void* b) {
+    //    return *static_cast<const int*>(a) - *static_cast<const int*>(b);
+    //});
+
+    //for (int value : v1)
+    //{
+    //    std::cout << value << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //std::for_each(v1.begin(), v1.end(), [](const int& val) {
+    //    std::cout << val << " ";
+    //});
+
+    //std::cout << std::endl;
+    //------------------------------------------------------------------
+
+    //std::array<int, 4> intArray = { 0, 1, 2, 3 };
+
+    //for (int value : intArray)
+    //{
+    //    std::cout << value << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //for (auto iter = intArray.begin(); iter != intArray.end(); ++iter)
+    //{
+    //    *iter = *iter + 1;
+    //    std::cout << *iter << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //for (int i = 0; i < intArray.size(); ++i)
+    //{
+    //    std::cout << intArray[i] << " ";
+    //}
+
+    //std::cout << std::endl;
+    //------------------------------------------------------------------
+
+    //std::vector<int> v1{ 1, 3, 2, 5, 4 };
+
+    //v1.push_back(6);
+
+    //for (int e : v1)
+    //{
+    //    std::cout << e << " ";
+    //}
+    //std::cout << std::endl;
+
+    //v1.pop_back();
+    //v1.resize(7, 0);
+    //v1.erase(v1.begin());
+
+    //for (int e : v1)
+    //{
+    //    std::cout << e << " ";
+    //}
+    //std::cout << std::endl;
+
+    //v1.clear();
+    //
+    //for (int e : v1)
+    //{
+    //    std::cout << e << " ";
+    //}
+
+    //------------------------------------------------------------------
+
+    //std::forward_list<int> list{ 1, 3, 5, 7 };
+
+    //auto itr = std::find(list.begin(), list.end(), 3);
+    //
+    //itr = list.insert_after(itr, 2);
+
+    //for (auto itr = list.begin(); itr != list.end(); ++itr)
+    //{
+    //    std::cout << *itr << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //std::forward_list<int> list2{ 3, 4, 6, 7 };
+    //list.splice_after(itr, list2);
+
+    //for (auto itr = list.begin(); itr != list.end(); ++itr)
+    //{
+    //    std::cout << *itr << " ";
+    //}
+
+    //std::cout << std::endl;
+
+    //list.sort();
+
+    //list.unique([](int a, int b) { return a == b; });
+
+    //list.erase_after(list.begin());
+
+    //for (auto itr = list.begin(); itr != list.end(); ++itr)
+    //{
+    //    std::cout << *itr << " ";
+    //}
+
+    //------------------------------------------------------------------
+
+    std::list<int> list{ 1, 2, 3 };
+
+    list.reverse();
+
+    for (int e : list)
     {
-        std::cout << value << " ";
+        std::cout << e << " ";
     }
-
     std::cout << std::endl;
 
-    std::vector<int> v1{ 1, 3, 2, 5, 4 };
+    std::list<int> list2{ 4, 5, 6 };
 
-    qsort(v1.data(), v1.size(), sizeof(v1[0]), [](const void* a, const void* b) {
-        return *static_cast<const int*>(a) - *static_cast<const int*>(b);
-    });
+    list.sort();
+    list2.merge(list);
 
-    for (int value : v1)
+    for (int e : list2)
     {
-        std::cout << value << " ";
+        std::cout << e << " ";
     }
-
     std::cout << std::endl;
 
-    std::for_each(v1.begin(), v1.end(), [](const int& val) {
-        std::cout << val << " ";
-    });
+    list2.erase(list2.begin());
+
 }
