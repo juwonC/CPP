d51e4c205aff2e0804f65c5cf74a0d42351a8062
JuwonCha <juwon1512@gmail.com>
2022-04-26 23:00:37 +0900

    Initial Commit

---
+++ ThreadPractice/ThreadPractice.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <thread>
+
+void PrintInt()
+{
+    for (int i = 0; i < 500; ++i)
+    {
+        std::cout << "Job1 : " << i << std::endl;
+    }
+}
+
+void PrintAscii()
+{
+    for (int repeat = 0; repeat < 5; ++repeat)
+    {
+        for (int i = 33; i < 126; ++i)
+        {
+            std::cout << "Job2 : " << (char)(i) << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    std::thread job1(PrintInt);
+    std::thread job2(PrintAscii);
+
+    job1.join();
+    job2.join();
+
+    std::cout << "--- All work has been completed ---" << std::endl;
+}
