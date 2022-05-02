94c17454dca54362c2d924b64a625c9ea3a4379d
JuwonCha <juwon1512@gmail.com>
2022-05-02 22:49:50 +0900

    Refactor Error Checking

--- FileStream/main.cpp
+++ FileStream/main.cpp
@@ -4,36 +4,26 @@
 bool LoadFile(const char* filename)
 {
 	std::ifstream ifs;
+	ifs.exceptions(std::ifstream::badbit | std::ifstream::failbit);
 
-	if (!ifs)
+	try
 	{
-		std::cout << "Cannot find the file" << std::endl;
+		ifs.open(filename);
+
+		char ch;
+		while (ifs.get(ch))
+		{
+			std::cout << ch;
+		}
+		ifs.close();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "There was an error running the file" << e.what() << std::endl;
+		ifs.close();
 		return false;
 	}
 
-	ifs.open(filename);
-
-	char ch;
-	while (ifs.get(ch))
-	{
-		if (!ifs.bad())
-		{
-			std::cerr << "The file is corrupt" << std::endl;
-			ifs.close();
-			return false;
-		}
-
-		if (!ifs.fail())
-		{
-			std::cerr << "Invalid file format" << std::endl;
-			ifs.close();
-			return false;
-		}
-
-		std::cout << ch;
-	}
-
-	ifs.close();
 	return true;
 }
 
