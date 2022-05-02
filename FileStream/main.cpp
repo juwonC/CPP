45443ab0d2ef583f52b3ee798c51922a5a3b12c3
JuwonCha <juwon1512@gmail.com>
2022-05-02 23:43:50 +0900

    Add Monster Class

--- FileStream/main.cpp
+++ FileStream/main.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
-bool LoadFile(const char* filename)
+class Monster
+{
+private:
+	std::string mName;
+	int mLevel;
+	int mHP;
+	int mMP;
+
+public:
+	Monster() : mName{ "" }, mLevel{}, mHP{}, mMP{}
+	{
+	}
+	Monster(std::string name, int level, int hp, int mp) : mName{ name }, mLevel{ level }, mHP{ hp }, mMP{ mp }
+	{
+	}
+
+public:
+	void SetName(std::string name) { mName = name; }
+	void SetLevel(int level) { mLevel = level; }
+	void SetHP(int hp) { mHP = hp; }
+	void SetMP(int mp) { mMP = mp; }
+};
+
+bool LoadFile(const std::string& filename, std::vector<Monster>& vector)
 {
 	std::ifstream ifs;
-	ifs.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+	ifs.exceptions(std::ifstream::badbit);
 
 	try
 	{
 		ifs.open(filename);
 
-		char ch;
-		while (ifs.get(ch))
+		std::string buffer;
+
+		std::getline(ifs, buffer);
+		while (!ifs.eof())
 		{
-			std::cout << ch;
+			Monster m;
+			
+			std::getline(ifs, buffer, ',');
+			m.SetName(buffer);
+			std::getline(ifs, buffer, ',');
+			m.SetLevel(std::stoi(buffer));
+			std::getline(ifs, buffer, ',');
+			m.SetHP(std::stoi(buffer));
+			std::getline(ifs, buffer);
+			m.SetMP(std::stoi(buffer));
+
+			vector.push_back(m);
 		}
 		ifs.close();
 	}
@@ -29,5 +67,7 @@ bool LoadFile(const char* filename)
 
 int main()
 {
-	LoadFile("Data/SimpleText.txt");
+	std::vector<Monster> monsterList;
+
+	LoadFile("Data/SimpleText.txt", monsterList);
 }
